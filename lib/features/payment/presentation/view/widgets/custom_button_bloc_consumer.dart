dadb436abd5b276483855d3cd0f21d962fbc4830
Ahmed-Elmekawy <elmekawy281@gmail.com>
2024-03-21 22:04:20 +0200

    integrate with paypal payment

--- lib/features/payment/presentation/view/widgets/custom_button_bloc_consumer.dart
+++ lib/features/payment/presentation/view/widgets/custom_button_bloc_consumer.dart
@@ -1,13 +1,18 @@
+import 'dart:developer';
+
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/functions/custom_snack_bar.dart';
 import 'package:bookly/core/functions/navigation.dart';
+import 'package:bookly/core/utils/api_keys.dart';
 import 'package:bookly/core/widgets/custom_burtton.dart';
 import 'package:bookly/features/payment/data/models/payment_intent_input_model.dart';
+import 'package:bookly/features/payment/data/models/paypal/input_paybal_model.dart';
 import 'package:bookly/features/payment/presentation/manager/payment_cubit.dart';
 import 'package:bookly/features/payment/presentation/manager/payment_states.dart';
 import 'package:bookly/features/payment/presentation/view/thank_you_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
   const CustomButtonBlocConsumer({
@@ -31,16 +36,48 @@ class CustomButtonBlocConsumer extends StatelessWidget {
       builder: (context, state) => CustomButton(
         color: kYellowColor,
         onPressed: () {
-          PaymentIntentInputModel paymentIntentInputModel =
-              PaymentIntentInputModel(
-                  customerId: "cus_PkxH0x4HgNOo6p",
-                  amount: "${total.ceil()}",
-                  currency: "EGP");
-          PaymentCubit.get(context).makePayment(paymentIntentInputModel);
+          isPaypal ? paypalPayment(context) : stripePayment(context);
         },
         text: "Continue",
         width: double.infinity,
       ),
     );
+  }
+
+  void paypalPayment(BuildContext context) {
+    ItemModelInput itemModelInput =
+        ItemModelInput(price: "${total.ceil()}", currency: "USD");
+    List<ItemModelInput> items = [itemModelInput];
+    InputPaypalModel inputPaypalModel = InputPaypalModel(
+        total: "${total.ceil()}", currency: "USD", items: items);
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (BuildContext context) => PaypalCheckoutView(
+        sandboxMode: true,
+        clientId: ApiKeys.clientId,
+        secretKey: ApiKeys.paypalSecretKey,
+        transactions: [inputPaypalModel.toJson()],
+        note: "Contact us for any questions on your order.",
+        onSuccess: (Map params) async {
+          log("onSuccess: $params");
+          navigateTo(context, widget: ThankYouView(total: total));
+        },
+        onError: (error) {
+          log("onError: $error");
+          Navigator.pop(context);
+        },
+        onCancel: () {
+          print('cancelled:');
+          Navigator.pop(context);
+        },
+      ),
+    ));
+  }
+
+  void stripePayment(BuildContext context) {
+    PaymentIntentInputModel paymentIntentInputModel = PaymentIntentInputModel(
+        customerId: "cus_PkxH0x4HgNOo6p",
+        amount: "${total.ceil()}",
+        currency: "EGP");
+    PaymentCubit.get(context).makePayment(paymentIntentInputModel);
   }
 }
